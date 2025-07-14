2f6afaeb0eab23deedaebc665a543353246a210d
Danielle <danielle.shokrian@aol.com>
2025-07-14 14:04:31 -0400

    Fixed function allAttempts to failedAttempts

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 function createLoginTracker(userInfo) {
   let attemptCount = 0;
-  let isLocked = false;
 
   const loginAttempt = (passwordAttempt) => {
-    if (isLocked) {
-      return 'Account locked due to too many failed login attempts';
-    }
-    attemptCount += 1;
-
-    if (attemptCount > 3) {
-      isLocked = true;
+    if (failedAttempts >= 3) {
       return 'Account locked due to too many failed login attempts';
     }
 
     if (passwordAttempt === userInfo.password) {
       return 'Login successful';
     } else {
-    return `Login failed`;
+      failedAttempts += 1;
+      if (failedAttempts >= 3) {
+        return 'Account locked due to too many failed login attempts';
+      }
+      return `Login failed`;
     }
   };
+
   return loginAttempt;
 }
 
