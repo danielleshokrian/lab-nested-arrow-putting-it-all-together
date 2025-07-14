79510e525ff3797d492e3082003410c60f835c2c
Danielle <danielle.shokrian@aol.com>
2025-07-14 13:58:07 -0400

    Login tracker that tracks how many wrong password attempts

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
+  let isLocked = false;
+
+  const loginAttempt = (passwordAttempt) => {
+    if (isLocked) {
+      return 'Account locked due to too many failed login attempts';
+    }
+    attemptCount += 1;
+
+    if (attemptCount > 3) {
+      isLocked = true;
+      return 'Account locked due to too many failed login attempts';
+    }
+
+    if (passwordAttempt === userInfo.password) {
+      return 'Login successful';
+    } else {
+    return `Login failed`;
+    }
+  };
+  return loginAttempt;
+}
+
+
 
 
 
